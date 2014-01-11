f057ba35e8853d71036ad0ee52d35287bcb32c70
Avi Romanoff <aviromanoff@gmail.com>
2014-01-10 23:12:41 -0500

    Okay so it works but i think we need to slow this relationship down :no_good:

--- models/user.js
+++ models/user.js
@@ -1,41 +1,57 @@
+/**
+ * A model for our user
+ */
 'use strict';
+var mongoose = require('mongoose'),
+    bcrypt = require('bcrypt'),
+    nconf = require('nconf');
 
 
-var userSchema = mongoose.Schema({
-    name: String,
-    login: { type: String, unique: true },
-    password: String
-});
+var userModel = function () {
 
-userSchema.pre('save', function (next) {
-    var user = this;
+        var userSchema = mongoose.Schema({
+            name: String,
+            login: { type: String, unique: true },  //Ensure logins are unique.
+            password: String //We'll store bCrypt hashed passwords.  Just say no to plaintext!
+        });
 
-    //If the password has not been modified in this save operation, leave it alone (So we don't double hash it)
-    if (!user.isModified('password')) {
-        next();
-        return;
-    }
+        /**
+         * Helper function that hooks into the 'save' method, and replaces plaintext passwords with a hashed version.
+         */
+        userSchema.pre('save', function (next) {
+            var user = this;
 
-    //Retrieve the desired difficulty from the configuration. (Default = 8)
-    var DIFFICULTY = (nconf.get('bcrypt') && nconf.get('bcrypt').difficulty) || 8;
+            //If the password has not been modified in this save operation, leave it alone (So we don't double hash it)
+            if (!user.isModified('password')) {
+                next();
+                return;
+            }
 
-    //Encrypt it using bCrypt. Using the Sync method instead of Async to keep the code simple.
-    var hashedPwd = bcrypt.hashSync(user.password, DIFFICULTY);
+            //Retrieve the desired difficulty from the configuration. (Default = 8)
+            var DIFFICULTY = (nconf.get('bcrypt') && nconf.get('bcrypt').difficulty) || 8;
 
-    //Replace the plaintext pw with the Hash+Salted pw;
-    user.password = hashedPwd;
+            //Encrypt it using bCrypt. Using the Sync method instead of Async to keep the code simple.
+            var hashedPwd = bcrypt.hashSync(user.password, DIFFICULTY);
 
-    //Continue with the save operation
-    next();
-});
+            //Replace the plaintext pw with the Hash+Salted pw;
+            user.password = hashedPwd;
 
-userSchema.methods.passwordMatches = function (plainText) {
-    var user = this;
-    return bcrypt.compareSync(plainText, user.password);
-}
+            //Continue with the save operation
+            next();
+        });
 
-module.exports = function UserModel() {
-    return {
-        name: 'overwork'
+        /**
+         * Helper function that takes a plaintext password and compares it against the user's hashed password.
+         * @param plainText
+         * @returns true/false
+         */
+        userSchema.methods.passwordMatches = function (plainText) {
+            var user = this;
+            return bcrypt.compareSync(plainText, user.password);
+        };
+
+
+        return mongoose.model('User', userSchema);
     };
-};
+
+module.exports = new userModel();
