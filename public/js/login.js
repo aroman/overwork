be59f2b96445e4f52f7faf47e1fe668cb54addf7
Rohan Mathur <rohanmathur34@gmail.com>
2014-01-12 22:24:25 -0500

    Kang sign up page styling + validation to log in page
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

---
+++ public/js/login.js
@@ -0,0 +1,30 @@
+// Dope hack so we don't have to re-validate
+// the entire form on every keystroke. <3 underscore
+invalid_inputs = _.map($(".form-control"), function (input) {
+    return $(input).attr('name');
+});
+
+var validate = function (event) {
+    var target = $(event.target);
+    var name = target.attr('name');
+    var val = target.val();
+
+    var error = "";
+
+    if (!validator.isEmail($(event.target).val())) {
+        error = "Invalid <i class='fa fa-frown-o'></i>";
+    }
+
+    if (error) {
+        target.parent().addClass("has-error");
+        invalid_inputs = _.uniq(_.union(invalid_inputs, [name]));
+    } else {
+        target.parent().removeClass("has-error");
+        invalid_inputs = _.without(invalid_inputs, name);
+    }
+
+    val && $(".error-" + name).html(error);
+    $("button[type=submit]").prop('disabled', !_.isEmpty(invalid_inputs));
+};
+
+$(".form-control").bind('input', validate);
